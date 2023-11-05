304daa775a661d5a708d0d3c809ba514ba156266
roLLlyTHuK <ircvipua@gmail.com>
2023-11-05 09:28:56 +0200

    upd style, gallery

--- src/index.js
+++ src/index.js
@@ -81,12 +81,7 @@ function renderContent(content) {
         <a href="${item.largeImageURL}" data-lightbox="image">
           <img src="${item.webformatURL}" alt="${item.tags}" loading="lazy" />
         </a>
-        <div class="info">
-          <p class="info-item"><b>Likes</b></br> ${item.likes}</p>
-          <p class="info-item"><b>Views</b></br> ${item.views}</p>
-          <p class="info-item"><b>Comments</b></br> ${item.comments}</p>
-          <p class="info-item"><b>Downloads</b></br> ${item.downloads}</p>
-        </div>
+        
       `;
     } else {
       card.innerHTML = `
@@ -97,12 +92,7 @@ function renderContent(content) {
           <source src="${item.videos.small.url}" type="video/mp4">
           Ваш браузер не поддерживает воспроизведение видео.
         </video>
-        <div class="info">
-          <p class="info-item"><b>Likes</b></br> ${item.likes}</p>
-          <p class="info-item"><b>Views</b></br> ${item.views}</p>
-          <p class="info-item"><b>Comments</b></br> ${item.comments}</p>
-          <p class="info-item"><b>Downloads</b></br> ${item.downloads}</p>
-        </div>
+        
       `;
     }
 
